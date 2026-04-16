df90682514daca65ae77e677254bed0654b5b696
yovarios22-ops <yovarios22@gmail.com>
2026-04-16 12:24:19 -0500

    portafolio terminado

--- scrip.js
+++ scrip.js
@@ -1,23 +1,23 @@
-const edad = 30;
-const esMayorEdad = true;
-const nombre = "Yovani";
-const precio = 99.99
 
-console.log(nombre, precio, edad);
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const mensaje = document.getElementById("mensaje");
 
-edad + 5;
-console.log(edad);
-console.log("suma: ", 5 + 5);
+//document.getElementById("formulario-contacto")
+//.addEventListener("click",function(e){
+ //   console.log("Hola hicieron clic en el formulario");
+//})
+document.getElementById("formulario-contacto")
+.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log("Escuchó un evento submit");
+    console.log(nombre.value);
+    console.log(correo.value);
+    console.log(mensaje.value);
+    Swal.fire({
+        title: "Registro exitoso!",
+        text: "Tus datos se mostraron correctamente!",
+        icon: "success"
+        });
+})
 
-//&& || !
-if(edad >=18){
-    console.log("mayor de edad");
-}
-else{
-    console.log("menor de edad");
-}
-
-const frutas =["manzana", "pera", "naranja", "fresa"]
-frutas.map((fruta)=>{
-        console.log(fruta);
-});
